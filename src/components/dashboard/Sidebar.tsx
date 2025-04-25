cb0864cc89b28636e2692702fbf7dcbe1a673773
Nicolas Gomez <nicolasgomez94@gmail.com>
2025-04-25 10:41:10 -0300

    diseño mejorado sidebar

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -51,12 +51,6 @@ const menuSections: MenuSection[] = [
 const Sidebar: React.FC = () => {
   return (
     <aside className="sidebar">
-      <div className="sidebar__logo">
-        <svg xmlns="http://www.w3.org/2000/svg" width="120" viewBox="0 0 200 50" fill="#ef4444">
-          <path d="M20,10 C14.5,10 10,14.5 10,20 C10,25.5 14.5,30 20,30 L30,30 L30,20 C30,14.5 25.5,10 20,10 Z M20,25 C17.2,25 15,22.8 15,20 C15,17.2 17.2,15 20,15 C22.8,15 25,17.2 25,20 L25,25 L20,25 Z"/>
-          <text x="40" y="25" fontSize="20" fontFamily="Arial" fontWeight="bold">CODEFEND</text>
-        </svg>
-      </div>
 
       <nav className="sidebar__nav">
         {menuSections.map((section, sectionIndex) => (
